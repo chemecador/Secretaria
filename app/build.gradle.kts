27921c9e5ed30e4095887462c7b3c5334a2240fc
Chemecador <chemecador@gmail.com>
2023-10-30 17:02:46 +0100

    Version Code 3 incluyendo ID de publicidad
    Exigencias de Play Console...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.chemecador.secretaria"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
