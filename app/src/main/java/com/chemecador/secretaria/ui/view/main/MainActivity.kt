5b3b5aafe4df9a59b8fc72f7ed32c8f2b074e3e9
chemecador <chemecador@gmail.com>
2024-05-24 23:45:14 +0200

    New colors (and night mode!)

---
+++ app/src/main/java/com/chemecador/secretaria/ui/view/main/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.chemecador.secretaria.ui.view.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.chemecador.secretaria.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding ?: throw IllegalStateException("Null binding")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+
+
+    private fun initUI() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //initListeners()
+        //initUIState()
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
