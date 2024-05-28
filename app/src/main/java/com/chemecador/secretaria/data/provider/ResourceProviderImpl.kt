8808e21f342f6b81748cc98018fe9044d408d4e3
chemecador <chemecador@gmail.com>
2024-05-28 21:37:33 +0200

    Code cleanup

--- app/src/main/java/com/chemecador/secretaria/data/provider/ResourceProviderImpl.kt
+++ app/src/main/java/com/chemecador/secretaria/data/provider/ResourceProviderImpl.kt
@@ -2,7 +2,6 @@ package com.chemecador.secretaria.data.provider
 
 import android.content.Context
 import androidx.annotation.StringRes
-import com.chemecador.secretaria.data.provider.ResourceProvider
 import javax.inject.Inject
 
 class ResourceProviderImpl @Inject constructor(private val context: Context) : ResourceProvider {
--- app/src/main/java/com/chemecador/secretaria/utils/DeviceUtils.kt
+++ app/src/main/java/com/chemecador/secretaria/utils/DeviceUtils.kt
@@ -1,51 +1,16 @@
 package com.chemecador.secretaria.utils
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import android.provider.Settings
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.core.app.ActivityCompat
 
 
 object DeviceUtils {
 
-
-    fun hasPermissions(context: Context, permissions: Array<String>): Boolean {
-        for (permission in permissions) {
-            if (permission.let {
-                    ActivityCompat.checkSelfPermission(
-                        context,
-                        it
-                    )
-                } != PackageManager.PERMISSION_GRANTED
-            ) {
-                return false
-            }
-        }
-        return true
-    }
-
-    @SuppressLint("HardwareIds")
-    fun getImei(context: Context): String {
-        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-    }
-
-    fun getVersionCode(context: Context): Long {
-        val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo.longVersionCode
-        } else {
-            @Suppress("DEPRECATION")
-            packageInfo.versionCode.toLong()
-        }
-    }
-
     fun hideKeyboard(activity: Activity) {
-        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         val vista = activity.currentFocus ?: View(activity)
         inputMethodManager.hideSoftInputFromWindow(vista.windowToken, 0)
     }
