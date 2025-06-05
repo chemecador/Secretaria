d4c52dbc5952aaaaa1ff3a810ea6db6d39268638
Chemecador <chemecador@gmail.com>
2025-06-05 23:35:19 +0200

    Version 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.chemecador.secretaria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.0.2"
+        versionCode = 22
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
